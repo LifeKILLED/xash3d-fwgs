5b96454968314e15ad24da03a50464c78a1476e4
LifeKILLED <frgrp@mail.ru>
2022-04-27 08:54:47 +0400

    rtx: reflections optimization - use SSR where you can

--- ref_vk/shaders/ray_light_direct.glsl
+++ ref_vk/shaders/ray_light_direct.glsl
@@ -8,9 +8,10 @@
 #include "ray_interop.h"
 #undef GLSL
 
-layout(set = 0, binding = 10, rgba32f) uniform readonly image2D position_t;
-layout(set = 0, binding = 11, rgba16f) uniform readonly image2D normals_gs;
-layout(set = 0, binding = 12, rgba8) uniform readonly image2D material_rmxx;
+layout(set = 0, binding = 10, rgba32f) uniform readonly image2D first_position_t;
+layout(set = 0, binding = 11, rgba32f) uniform readonly image2D position_t;
+layout(set = 0, binding = 12, rgba16f) uniform readonly image2D normals_gs;
+layout(set = 0, binding = 13, rgba8) uniform readonly image2D material_rmxx;
 
 #define X(index, name, format) layout(set=0,binding=index,format) uniform writeonly image2D out_image_##name;
 OUTPUTS(X)
@@ -40,10 +41,46 @@ void readNormals(ivec2 uv, out vec3 geometry_normal, out vec3 shading_normal) {
 	shading_normal = normalDecode(n.zw);
 }
 
+#if REUSE_SCREEN_LIGHTING
+
+vec3 OriginWorldPosition(mat4 inv_view) {
+	return (inv_view * vec4(0, 0, 0, 1)).xyz;
+}
+
+
+vec3 ScreenToWorldDirection(vec2 uv, mat4 inv_view, mat4 inv_proj) {
+	vec4 target    = inv_proj * vec4(uv.x, uv.y, 1, 1);
+	vec3 direction = (inv_view * vec4(normalize(target.xyz), 0)).xyz;
+	return normalize(direction);
+}
+
+
+vec3 WorldPositionFromDirection(vec3 origin, vec3 direction, float depth) {
+	return origin + normalize(direction) * depth;
+}
+
+vec3 FarPlaneDirectedVector(vec2 uv, vec3 forward, mat4 inv_view, mat4 inv_proj) {
+	vec3 dir = ScreenToWorldDirection(uv, inv_view, inv_proj);
+	float plane_length = dot(forward, dir);
+	return dir / max(0.001, plane_length);
+}
+
+vec2 WorldPositionToUV(vec3 position, mat4 proj, mat4 view) {
+	vec4 clip_space = proj * (view * vec4(position, 1.));
+	return clip_space.xy / clip_space.w;
+}
+
+ivec2 UVToPix(vec2 uv, ivec2 res) {
+	vec2 screen_uv = uv * 0.5 + vec2(0.5);
+	return ivec2(screen_uv.x * float(res.x), screen_uv.y * float(res.y));
+}
+
+#endif // REUSE_SCREEN_LIGHTING
+
 void main() {
 	const vec2 uv = (gl_LaunchIDEXT.xy + .5) / gl_LaunchSizeEXT.xy * 2. - 1.;
 	const ivec2 pix = ivec2(gl_LaunchIDEXT.xy);
-
+	
 	rand01_state = ubo.random_seed + gl_LaunchIDEXT.x * 1833 +  gl_LaunchIDEXT.y * 31337;
 
 	// FIXME incorrect for reflection/refraction
@@ -60,12 +97,37 @@ void main() {
 
 	const vec3 pos = imageLoad(position_t, pix).xyz;
 
-	vec3 geometry_normal, shading_normal;
-	readNormals(pix, geometry_normal, shading_normal);
-
-	const vec3 throughput = vec3(1.);
 	vec3 diffuse = vec3(0.), specular = vec3(0.);
-	computeLighting(pos + geometry_normal * .001, shading_normal, throughput, -direction, material, diffuse, specular);
+
+#if REUSE_SCREEN_LIGHTING
+
+	const ivec2 res = ivec2(imageSize(first_position_t));
+	const vec2 first_uv = WorldPositionToUV(pos, inverse(ubo.inv_proj), inverse(ubo.inv_view));
+	const ivec2 first_pix = UVToPix(first_uv, res);
+	uint lighting_is_reused = 0;
+
+	if (any(greaterThanEqual(first_pix, ivec2(0))) && any(lessThan(first_pix, res))) {
+		const vec3 first_pos = imageLoad(first_position_t, first_pix).xyz;
+		const vec3 origin = (ubo.inv_view * vec4(0, 0, 0, 1)).xyz;
+		const float nessesary_depth = length(origin - pos);
+		const float current_depth = length(origin - first_pos);
+		if (abs(nessesary_depth - current_depth) < 2.) {
+			lighting_is_reused = 1;
+			diffuse = vec3(first_uv, -100.);
+		}
+	}
+
+	if (lighting_is_reused == 0) {
+	#else
+	{
+	#endif // REUSE_SCREEN_LIGHTING
+
+		vec3 geometry_normal, shading_normal;
+		readNormals(pix, geometry_normal, shading_normal);
+		const vec3 throughput = vec3(1.);
+		computeLighting(pos + geometry_normal * .001, shading_normal, throughput, -direction, material, diffuse, specular);
+
+	}
 
 #if GLOBAL_ILLUMINATION
 #if LIGHT_POINT
