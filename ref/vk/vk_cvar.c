da679a1e020c2ff5537d04d3445c09ac8db4d75a
Grigoriy Kabanov <frgrp@mail.ru>
2025-11-02 17:11:04 +0400

    vk: add rt_upscale_fxaa cvar and render in half resolution

--- ref/vk/vk_cvar.c
+++ ref/vk/vk_cvar.c
@@ -46,6 +46,7 @@ void VK_LoadCvarsAfterInit( void )
 		rt_denoise_gi_by_sh = gEngine.Cvar_Get("rt_denoise_gi_by_sh", "", FCVAR_GLCONFIG, "Denoise global illumination by spherical harmonics");
 		rt_disable_gi = gEngine.Cvar_Get("rt_disable_gi", "", FCVAR_GLCONFIG, "Disable global illumination calculation");
 		rt_spatial_reconstruction = gEngine.Cvar_Get("rt_spatial_reconstruction", "", FCVAR_GLCONFIG, "Apply spatial reconstruction to specular");
+		rt_upscale_fxaa = gEngine.Cvar_Get("rt_upscale_fxaa", "", FCVAR_GLCONFIG, "Render in low resolution and apply FXAA");
 	} else {
 		rt_enable = gEngine.Cvar_Get( "rt_enable", "0", FCVAR_READ_ONLY, "DISABLED: Ray tracing is not supported by your hardware/drivers" );
 	}
